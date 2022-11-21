b5e29a785dbd26020fb1b29ae16c9fc3bd45f85a
szlashaska <kamilpetryniak@gmail.com>
2022-11-21 12:15:18 +0100

    firebase

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,34 @@
 import Add from "../assets/addAvatar.png";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+
 const Register = () => {
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  };
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Chat app</span>
         <span className="title">Register</span>
-        <form>
+        <form onSubmit={handleSumbit}>
           <input type="text" placeholder="display name" />
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
